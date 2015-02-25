7c682687c351bda88b742d04414b59d56748a1ab
Bryan CS <crappylabs@gmail.com>
2015-02-25 10:50:54 +0800

    Unified query

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var pageAccessToken = {access_token : ''};
-$(document).ready(function() {
+$(document).ready(function() {      
       $.ajaxSetup({ cache: true });
         $.getScript('//connect.facebook.net/en_UK/all.js', function(){
         
@@ -14,48 +14,41 @@ $(document).ready(function() {
             pageAccessToken = {access_token : response.authResponse.access_token}
           });            
           // FB.api('/'+albumId+'/photos?fields=data', pageAccessToken, function(response) { //faster
-          FB.api('/'+albumId+'/photos', pageAccessToken, function(response) {                
+          FB.api('/'+albumId+'/photos', pageAccessToken, function(response) {                      
             //set title
             $('#header').append(response.data[0].from.name + ' Standings');                
             
             for(var i=0;i<response.data.length;i++){                  
-              showCandidates(response.data[i].id, pageAccessToken);                  
-            }
-          });
-                                                                         
+              showStandings(response.data[i].id, pageAccessToken);                  
+            }            
+          });                                                                      
         });
-    var showLikeCounts = function (candidateId, pageAccessToken){             
-      FB.api('/'+candidateId+'?fields=id,likes.limit(1).summary(true)', pageAccessToken, _callback);          
+
+    var showStandings = function (candidateId, pageAccessToken){             
+      FB.api('/'+candidateId+'?fields=id,source,link,likes.limit(0).summary(true)', pageAccessToken, callBack);
     }
-    var showCandidates = function (candidateId, pageAccessToken){             
-      FB.api('/'+candidateId, pageAccessToken, _callback);                        
-    }        
+      
     var counter = 0;
     var profile = '';
+    
 
-    var _callback = function(response){      
-
-      if (response.source) {//show candidates    
+    var callBack = function(response){    
+                
+        var totalCount = response.likes.summary.total_count;        
         var link = response.link;
-        profile += '<div class="col-xs-6 col-md-4"><div id='+response.id+' class="candidates">';
-        profile += '<a href='+link+' target="_blank"><img src='+response.source+' /><\/a>';
-        profile += '<\/div><\/div>';
-        counter++;
 
+        profile += '<div class="col-xs-6 col-md-4"><div id='+response.id+' class="candidates">';
+        profile += '<a href='+link+' target="_blank"><img src='+response.source+' class="img-responsive" /></a>';
+        profile += '<div><i class="fa fa-thumbs-o-up" style="color:blue;"></i>  <b>'+totalCount+'</b></div>';
+        profile += '</div></div>';                      
+        counter++;
         if(counter % 3 == 0){
           var row = '<div class="row">'+profile+'<\/div>';              
           $('.container-fluid').append(row); 
-                    
+          
           //clear profile
-          profile = '';              
-        }
-        
-        showLikeCounts(response.id, pageAccessToken);
-        
-      }else{//show likes           
-        var totalCount = response.likes.summary.total_count;            
-        $('#'+response.id).append('<div><i class="fa fa-thumbs-o-up" style="color:blue;"></i>  <b>'+totalCount+'</b></div>');
-      };
-              
+          profile = '';                      
+        }          
+                               
     };
   });
